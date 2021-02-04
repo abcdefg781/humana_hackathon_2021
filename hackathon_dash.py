64c2c517a304cfa60575263b23624cc988ee22ba
Adeline Shin <ashin@humana.com>
2021-02-04 15:20:49 -0500

    adding survey questions/dropdowns and deck
    Signed-off-by: Adeline Shin <ashin@humana.com>

--- hackathon_dash.py
+++ hackathon_dash.py
@@ -15,9 +15,10 @@ import dash_html_components as html
 import random
 # import dash_table
 # import numpy as np
-# import pandas as pd
+import pandas as pd
 # import plotly.graph_objects as go
 from dash.dependencies import Input, Output, State
+
 
 ################################################################
 # Dash App Setup and Style Sheets
@@ -30,7 +31,7 @@ warnings.filterwarnings("ignore")
 
 ################################################################
 # Import Data
-
+states_df = pd.read_csv("./data/states.csv")
 ################################################################
 # Functions Within App
 
@@ -77,23 +78,76 @@ app.layout = dbc.Container(
                     of your mental health status and provides resources for your mental health during the pandemic. To
                     complete the assessment, please answer the following questions below.
                     >
-                    > Disclaimer: This tool is meant to be used as a risk assessment tool for self-awareness. It is not
-                    > meant to replace a professional assessment or a psychiatric evaluation. Please not that the
+                    > **Disclaimer**: This tool is meant to be used as a risk assessment tool for self-awareness. It is not
+                    > meant to replace a professional assessment or a psychiatric evaluation. Please note that the
                     > number you receive does not define you and can change based on a number of circumstances.
                     >
                     '''),
-                    html.P(
-                        ""),
+                    # Name
+                    html.P("What is your name?"),
                     dbc.Input(type="text", id="example-name", placeholder="Name"),
                     html.Br(),
-                    dbc.Input(type="text", id="example-zip", placeholder="Zip Code"),
-                    # dcc.Dropdown(className='div-for-dropdown', id='standings_year', value=2020, clearable=False,
-                    #              options=[{'label': i, 'value': i} for i in races_df['year'].unique()]),
+                    # Q1: State
+                    html.P("In what state do you currently reside?"),
+                    dcc.Dropdown(className='div-for-dropdown', id="q1", clearable=True,
+                                 options=[{'label': i, 'value': i} for i in states_df['State']]),
+                    html.Br(),
+                    # Q2: County
+                    html.P("In what county do you currently reside?"),
+                    dbc.Input(type="text", id="q2", placeholder="County Name"),
+                    html.Br(),
+                    # Q3: Age
+                    html.P("What is your age?"),
+                    dbc.Input(type="number", id="q3", placeholder="Age"),
+                    html.Br(),
+                    # Q4: Gender
+                    html.P("What is your gender?"),
+                    dcc.Dropdown(className='div-for-dropdown', id="q4", clearable=True,
+                                 options=[
+                                      {'label': 'Male', 'value': 0},
+                                      {'label': 'Female', 'value': 1},
+                                      {'label': 'Transgender Female', 'value': 2},
+                                      {'label': 'Transgender Male', 'value': 3},
+                                      {'label': 'Non-Binary', 'value': 4}
+                                  ]),
+                    html.Br(),
+                    # Q5: Children
+                    html.P("How many children do you have?"),
+                    dbc.Input(type="number", id="q5", placeholder="Number of Children"),
+                    html.Br(),
+                    # Q6: Smoking
+                    html.P("Do you currently smoke?"),
+                    dcc.Dropdown(className='div-for-dropdown', id="q6", clearable=False,
+                                 options=[
+                                     {'label': 'Yes', 'value': 1},
+                                     {'label': 'No', 'value': 0}
+                                 ]),
+                    html.Br(),
+                    # Q7: Work Hours
+                    html.P("In the past week, how many hours were you working for pay?"),
+                    dbc.Input(type="number", id="q7", placeholder="Number of Working Hours"),
+                    html.Br(),
+                    # Q8: Happy Days
+                    html.P("On a scale of 0-10, how happy would you say you are, generally?"),
+                    dbc.Input(type="number", id="q8", placeholder="Please enter a number from 0-10"),
+                    html.Br(),
+                    # Q9: Mentally Unhealthy Days
+                    html.P("In the past 30 days, how many days have you felt mentally unhealthy?"),
+                    dbc.Input(type="number", id="q9", placeholder="Please enter a number less than 30"),
+                    html.Br(),
+                    # Q10: Anxiety Increase
+                    html.P("In the past 30 days, have you felt more anxious than previously?"),
+                    dcc.Dropdown(className='div-for-dropdown', id="q10", clearable=False,
+                                 options=[
+                                     {'label': 'Yes', 'value': 1},
+                                     {'label': 'No', 'value':0}
+                                 ]),
                     html.Br(),
                     html.Button(id='submit-button', n_clicks=0, children='Submit'),
                     html.Br(),
                     html.Br(),
                     html.Div(id='outcome'),
+                    html.Br()
                 ]),
                 # Tab 2: Resources
                 dbc.Tab(label="Resources", tab_id="resources", children=[
@@ -137,6 +191,16 @@ app.layout = dbc.Container(
                         meaningful conversations with someone new
                         * Find a [Mental Health Provider](https://www.humana.com/logon/) in your area
                     '''),
+                ]),
+                dbc.Tab(label="Why Minds Matter", tab_id="deck", children=[
+                    html.Br(),
+                    dcc.Markdown('''## Problems We Face'''),
+                    html.Img(src=app.get_asset_url("deck1.png")),
+                    dcc.Markdown('''
+                    Text here. Behavioral Health is the cornerstone of any public health efforts.
+                    '''),
+                    dcc.Markdown('''## Solution We Propose'''),
+                    html.Img(src=app.get_asset_url("deck2.png")),
                 ]),
                 dbc.Tab(label="Our Team", tab_id="team", children=[
                     html.Br(),
@@ -188,9 +252,18 @@ app.layout = dbc.Container(
     Output(component_id='outcome', component_property='children'),
     [Input(component_id='submit-button', component_property='n_clicks')],
     [State(component_id='example-name', component_property='value'),
-     State(component_id='example-zip', component_property='value')]
+     State(component_id='q1', component_property='value'),
+     State(component_id='q2', component_property='value'),
+     State(component_id='q3', component_property='value'),
+     State(component_id='q4', component_property='value'),
+     State(component_id='q5', component_property='value'),
+     State(component_id='q6', component_property='value'),
+     State(component_id='q7', component_property='value'),
+     State(component_id='q8', component_property='value'),
+     State(component_id='q9', component_property='value'),
+     State(component_id='q10', component_property='value')]
 )
-def get_output(n_clicks, name_input, zip_input):
+def get_output(n_clicks, name_input, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10):
     click_value = n_clicks
     if click_value is None:
             raise dash.exceptions.PreventUpdate
